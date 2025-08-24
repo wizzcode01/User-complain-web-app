c48e4a32f5e841e99059e83659da0e94799611a6
wizzycode <dicksonwisdom005@gmail.com>
2025-08-24 17:42:30 +0100

    afterHero section fix

--- src/components/AfterHero.jsx
+++ src/components/AfterHero.jsx
@@ -13,7 +13,7 @@ const AfterHero = ({ afterContent }) => {
         <>
         <div className="relative">
             <img className="w-32 absolute mb-10 right-20 border-2 border-blue-600 rounded-full translate-x-0 rotate-45 lg:visible hidden" src={img} alt="complain1" />
-          <div className="flex p-2 gap-4 w-full lg:flex-row flex-col justify-center items-center lg:h-0 h-96 md:h-96"> 
+          <div className="flex p-2 gap-4 w-full lg:flex-row flex-col justify-center items-center lg:h-full h-96 md:h-96"> 
             <img className="lg:w-[50%] w-full" src={img3}/>
             <p className="w-80 font-normal text-gray-700 text-lg">{afterContent}</p>
           </div> 
